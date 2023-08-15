aca80260f17adfe83f8aa8344176111c54488aa3
Kishan Kumar Gupta <81075502+kishan-1000@users.noreply.github.com>
2023-08-15 14:35:35 +0530

    logic.js

---
+++ logic.js
@@ -0,0 +1,4 @@
+var name1 = prompt("Enter your name : ");
+var name2 = prompt("Enter your partner name :");
+var percent = Math.floor(Math.random()*100+1)
+console.log("your love score is "+percent+"%");
